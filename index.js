98c640aa31da2f1e35ce95597e299f160291e88c
andreluisw059 <andreluiswenningkamp52@gmail.com>
2025-03-27 13:22:00 -0300

    Adicionando novas funcionalidades de filtrar locais para alugar

--- index.js
+++ index.js
@@ -4,23 +4,35 @@ const readline = require("readline");
 const leitor = readline.createInterface(process.stdin, process.stdout);
 
 const lerDados = async(numeroHospedes) => {
-  let resultado = '';
+  const dadosReserva = await csv.leCsv(csv.linkCsv.reserva);
+  const resultadoDosDadosReserva = dadosReserva.dados;
   const dadosCatalogo = await csv.leCsv(csv.linkCsv.catalogo);
-  dadosCatalogo.forEach((imovel) => {
-    if(Number(imovel.capacidade_de_pessoas) > Number(numeroHospedes)) {
-      resultado = imovel;
-    } else {
-      resultado = 'indisponivel'
+  const resultadoDosDadosCatalogo = dadosCatalogo.dados;
+  let preco = [];
+  let codigo = [];
+  let possibilidadeDeLocacao = false;
+  resultadoDosDadosCatalogo.filter(({ capacidade_de_pessoas, preco_por_dia, codigo_do_imovel }) => {
+    preco.push(preco_por_dia);
+    if (Number(capacidade_de_pessoas) >= Number(numeroHospedes)) {
+      codigo.push(codigo_do_imovel);
+      return possibilidadeDeLocacao = true;
     }
   });
-  return resultado;
+
+  resultadoDosDadosReserva.filter((element, index) => {
+    if (Number(element.numero_de_pessoas) <= Number(numeroHospedes) && possibilidadeDeLocacao) {
+     console.log(`Código do imóvel disponível: ${element.codigo_do_imovel}\nData: ${new Date(element.data_de_saida).toLocaleDateString()}\nDiária: R$ ${element.codigo_do_imovel === codigo[index] ? preco[index] : 0}`);
+    }
+  });
+  if (!possibilidadeDeLocacao) {
+    console.log(`Nenhum local disponpivel para ${numeroHospedes} pessoas.`);
+  }  
 }
 
-
 const menu = () => {
-  leitor.question('Digite quantas pessoas querem se hospedar ',  (resposta) => {
-    let teste = lerDados(resposta);
-    console.log(teste);
+  leitor.question('Digite quantas pessoas querem se hospedar ', async(resposta) => {
+    await lerDados(resposta);
+    leitor.close();
   });
   }
 
--- scripts/lerCsv.js
+++ scripts/lerCsv.js
@@ -2,22 +2,19 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const catalogo = './data/catalogo';
 const reserva = './data/reserva';
-const resultado = [];
-
-
-converteCsv = (resultado) => {
-  return resultado;
- }
+let resultado = {
+  dados: []
+};
 
 const leCsv = async (arquivo) => {
   return new Promise((resolve, reject) => {
     fs.createReadStream(`${arquivo}.csv`)
     .pipe(csv())
     .on('data', (row) => {
-      resultado.push(row);
+      resultado.dados.push(row);
     })
     .on('end', () => {
-      resolve(resultado)
+      resolve(resultado);
     })
   }
   )
